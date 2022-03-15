c96a0f60271081beedb7b80cd9d63bb2b5c78ca1
marcin <marcingadz@gmail.com>
2022-03-15 17:22:10 +0100

    Add routing service based on Spring Cloud Gateway

--- startup.sh
+++ startup.sh
@@ -8,11 +8,14 @@ echo -e "\n Order-service..."
 cd "$starting"/order-service && mvn clean package -DskipTests=true
 echo -e "\n Eureka-service..."
 cd "$starting"/eureka-server && mvn clean package -DskipTests=true
+echo -e "\n cloud-gateway..."
+cd "$starting"/cloud-gateway && mvn clean package -DskipTests=true
 cd "$starting"
 
 # build images with latest version of services
 echo -e "\n\nBuilding images..."
 cd "$starting"/user-service && docker build -t user-service:current .
+cd "$starting"/cloud-gateway && docker build -t cloud-gateway:current .
 cd "$starting"/order-service && docker build -t order-service:current .
 cd "$starting"/eureka-server && docker build -t eureka-server:current .
 echo -e "\n\nRunning compose..."
